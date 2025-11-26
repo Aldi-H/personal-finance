6a7cb06bfdeab99651bc7535642844e1f66b07af
Aldi-H <aldihimawan88@gmail.com>
2025-11-26 15:10:24 +0700

    style: Refactor button styles and update variants for improved UI consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,60 @@
+"use client";
+
+import {
+  CaretRightIcon,
+  CubeIcon,
+  HeartIcon,
+  HorseIcon,
+} from "@phosphor-icons/react";
 import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
-      <div className="text-preset-1 text-cool-steel">
-        Phasellus ultrices nulla quis nibh
-      </div>
-
-      <div className="text-preset-2">Phasellus ultrices nulla quis nibh</div>
-
-      <div className="text-preset-3">Phasellus ultrices nulla quis nibh</div>
-
-      <div className="text-preset-4">
-        Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer
-        ligula vulputate sem tristique cursus. Nam nulla quam, gravida non,
-        commodo a, sodales sit amet, nisi.
-      </div>
-
-      <div className="text-preset-4-bold">
-        Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer
-        ligula vulputate sem tristique cursus. Nam nulla quam, gravida non,
-        commodo a, sodales sit amet, nisi.
-      </div>
-
-      <div className="text-preset-5">
-        Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer
-        ligula vulputate sem tristique cursus. Nam nulla quam, gravida non,
-        commodo a, sodales sit amet, nisi.
-      </div>
-
-      <div className="text-preset-4-bold">
-        Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer
-        ligula vulputate sem tristique cursus. Nam nulla quam, gravida non,
-        commodo a, sodales sit amet, nisi.
-      </div>
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/5 px-1 py-0.5 font-semibold dark:bg-white/6">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <Button>Placeholder</Button>
+        <Button variant="secondary">Placeholder</Button>
+        <Button variant="destroy">Placeholder</Button>
+        <Button variant="tertiary">
+          Placeholder <CaretRightIcon size={16} weight="fill" />
+        </Button>
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm font-medium text-background transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/8 px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <HorseIcon />
+        <HeartIcon color="#AE2983" weight="fill" size={32} />
+        <CubeIcon color="teal" weight="duotone" />
+
+        <div className="text-preset-1 text-cool-steel">
+          Phasellus ultrices nulla quis nibh
+        </div>
+
+        <div className="text-preset-2">Phasellus ultrices nulla quis nibh</div>
+
+        <div className="text-preset-3">Phasellus ultrices nulla quis nibh</div>
+
+        <div className="text-preset-4">
+          Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
+          consectetuer ligula vulputate sem tristique cursus. Nam nulla quam,
+          gravida non, commodo a, sodales sit amet, nisi.
+        </div>
+
+        <div className="text-preset-4-bold">
+          Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
+          consectetuer ligula vulputate sem tristique cursus. Nam nulla quam,
+          gravida non, commodo a, sodales sit amet, nisi.
+        </div>
+
+        <div className="text-preset-5">
+          Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
+          consectetuer ligula vulputate sem tristique cursus. Nam nulla quam,
+          gravida non, commodo a, sodales sit amet, nisi.
+        </div>
+
+        <div className="text-preset-4-bold">
+          Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
+          consectetuer ligula vulputate sem tristique cursus. Nam nulla quam,
+          gravida non, commodo a, sodales sit amet, nisi.
         </div>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,31 +4,26 @@ import { type VariantProps, cva } from "class-variance-authority";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "text-preset-4-bold inline-flex shrink-0 items-center justify-center gap-2 rounded-xl font-bold tracking-normal transition-all focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
+        primary: "bg-shadow-grey text-white shadow-xs hover:bg-dim-grey",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-parchment shadow-xs outline-parchment hover:bg-white hover:outline-1",
+        destroy: "bg-rosy-copper text-white shadow-xs hover:bg-rosy-copper/80",
+        tertiary:
+          "bg-transparent text-dim-grey shadow-none hover:text-shadow-grey",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "h-[53px] p-4 has-[>svg]:px-3",
         sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   },
@@ -49,7 +44,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, className }), "")}
       {...properties}
     />
   );
