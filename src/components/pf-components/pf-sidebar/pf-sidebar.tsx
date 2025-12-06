0e893b5f977b1a293d81b0188c395c2e02a5bc62
Aldi-H <aldihimawan88@gmail.com>
2025-12-06 17:37:05 +0700

    feat: refactor navigation components and consolidate nav data into config

--- src/components/pf-components/pf-sidebar/pf-sidebar.tsx
+++ src/components/pf-components/pf-sidebar/pf-sidebar.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  ArrowsDownUpIcon,
-  ChartDonutIcon,
-  HouseIcon,
-  ReceiptIcon,
-  TipJarIcon,
-} from "@phosphor-icons/react";
+import { sidebarNav } from "@/config/navigation";
 import React from "react";
 
 import {
@@ -20,38 +14,6 @@ import {
 import PfSidebarHeader from "./pf-sidebar-header";
 import PfSidebarMain from "./pf-sidebar-main";
 
-const navData = {
-  main: [
-    {
-      title: "Overview",
-      url: "#",
-      icon: HouseIcon,
-      // isActive: true,
-    },
-    {
-      title: "Transactions",
-      url: "#",
-      icon: ArrowsDownUpIcon,
-      isActive: true,
-    },
-    {
-      title: "Budgets",
-      url: "#",
-      icon: ChartDonutIcon,
-    },
-    {
-      title: "Pots",
-      url: "#",
-      icon: TipJarIcon,
-    },
-    {
-      title: "Recurring bills",
-      url: "#",
-      icon: ReceiptIcon,
-    },
-  ],
-};
-
 const PfSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
   return (
     <Sidebar collapsible="icon" {...props} className="">
@@ -60,7 +22,7 @@ const PfSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
       </SidebarHeader>
 
       <SidebarContent className="pt-6">
-        <PfSidebarMain items={navData.main} />
+        <PfSidebarMain items={sidebarNav} />
       </SidebarContent>
 
       <SidebarFooter className="px-8 py-4">
