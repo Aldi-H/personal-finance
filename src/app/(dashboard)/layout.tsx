5a9a42a386e329190a016f111d48124096a62b57
Aldi-H <aldihimawan88@gmail.com>
2025-12-19 10:20:52 +0700

    style: Refactor Dashboard layout to include SidebarInset and PfBottomNav

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,19 @@
+import { mobileNav } from "@/config/navigation";
 import React from "react";
 
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
+import PfBottomNav from "@/components/pf-components/pf-navigation-menu/pf-navigation-menu";
 import PfSidebar from "@/components/pf-components/pf-sidebar/pf-sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <PfSidebar />
-      {children}
+      <PfBottomNav navItems={mobileNav} />
+      <SidebarInset>
+        <div className="m-10">{children}</div>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
