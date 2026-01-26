39fb535487094040e235b266046d4ddd51f1215c
Aldi-H <aldihimawan88@gmail.com>
2026-01-26 14:33:45 +0700

    feat: add transactions page with header component

---
+++ src/app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,11 @@
+import PfPageHeader from "@/components/pf-components/pf-header/pf-page-header";
+
+const PageTransactions = () => {
+  return (
+    <div>
+      <PfPageHeader />
+    </div>
+  );
+};
+
+export default PageTransactions;
